62d2d0780a03d4519f296471effea5cbf74b0708
Poloru <126425768+Poloru@users.noreply.github.com>
2023-10-29 21:06:01 +0100

    Delete index.test.js

--- index.test.js
+++
@@ -1,6 +0,0 @@
-const getReverseString = require('./index');
-
-describe("Тест функции getReverseString", () => {
-  it('Должен перевернуть строку "abc"', () => expect(getReverseString("abc")).toBe("cba"));
-  it('Должен перевернуть строку "qwerty"', () => expect(getReverseString("qwerty")).toBe("ytrewq"));
-});
